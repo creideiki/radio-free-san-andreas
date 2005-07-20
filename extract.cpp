0bd189aec3bdad53efa035376843dbbfd5674395
Karl-Johan Karlsson <creideiki@ferretporn.se>
2005-07-20 22:39:02 +0000

    Basic functionality complete - extracts Ogg Vorbis files. No metadata yet.

--- extract.cpp
+++ extract.cpp
@@ -1,5 +1,5 @@
-// Copyright (C) 2005 Karl-Johan Karlsson <creideiki@ferretporn.se>
-// A program for extracting radio tracks from Grand Theft Auto - San Andreas.
+// San Andreas Audio Extractor - Extracts radio tracks from Grand Theft Auto - San Andreas.
+// Copyright (C) 2005 Karl-Johan Karlsson <creideiki+san-andreas@ferretporn.se>
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,3 +18,223 @@
 // $Id$
 // $HeadURL$
 
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cstdio>
+
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+using namespace std;
+
+/* Exit codes:
+ * 128 - command line error - usage information printed
+ * 1   - error accessing target directory
+ * 2   - error accessing source files
+ * 4   - error writing output files
+ * 8   - sanity check of file contents failed
+ */
+
+//These structs are for demuxing the Ogg streams.
+struct ogg_entry
+{
+  uint32_t size;
+  uint32_t foo;
+};
+
+struct ogg_header
+{
+  uint8_t unused1[8000];
+  ogg_entry e[8];
+  uint8_t unused2[4];
+};
+
+void read_header(const uint8_t *source, ogg_header *dest)
+{
+   memcpy(dest, source, sizeof(ogg_header));
+}
+
+void print_usage()
+{
+   cerr << "Usage:\n"
+        << "  extract [-v] <file1> [<file2> ...] <target_dir>\n\n"
+
+        << "Extracts Ogg Vorbis audio files from the Grand Theft Auto -\n"
+        << "San Andreas data files <file1>, <file2>, ... and stores the\n"
+        << "resulting files in directory <target_dir>.\n\n" << endl;
+}
+
+int main(int argc, char **argv)
+{
+   if(argc < 3)
+   {
+      print_usage();
+      exit(128);
+   }
+
+   struct stat stat_buffer;
+   int result;
+   result = stat(argv[argc - 1], &stat_buffer);
+   if(result == -1)
+   {
+      cerr << "Error accessing target directory: " << strerror(errno) << endl;
+      exit(1);
+   }
+   if(!S_ISDIR(stat_buffer.st_mode))
+   {
+      cerr << "Error accessing target directory: Not a directory." << endl;
+      exit(1);
+   }
+
+   for(int infile = 1; infile < argc - 1; ++infile)
+   {
+      string infilename = argv[infile];
+      string::size_type last_slash = infilename.rfind('/');
+      if(last_slash == string::npos)
+      {
+         last_slash = 0;
+      }
+      else
+      {
+         ++last_slash;
+      }
+      string inbasename = infilename.substr(last_slash);
+
+      cout << "Processing file: " << infilename << endl;
+         
+      result = stat(argv[infile], &stat_buffer);
+      if(result == -1)
+      {
+         cerr << "Error accessing input file " << argv[infile] << ": "
+              << strerror(errno) << endl;
+         exit(2);
+      }
+      int file_length = stat_buffer.st_size;
+
+      int in_fd = open(argv[infile], O_RDONLY);
+      if(in_fd == -1)
+      {
+         cerr << "Error opening input file " << argv[infile] << ": "
+              << strerror(errno) << endl;
+         exit(2);
+      }
+
+      //Read the entire file into memory. The largest file is only
+      //128MB, which should fit in memory without problems on a
+      //machine that can run GTA:SA. Note that the buffer is aligned
+      //for a uint32_t, which will help us get more speed later. This
+      //bit-fiddling will work as long as an uint8_t does not have
+      //more stringent alignment requirements than an uint32_t. I know
+      //of no processor that does anything else.
+      uint32_t *file_image = new uint32_t[(file_length / 4) + 1];
+      result = read(in_fd, file_image, file_length);
+      if(result == -1)
+      {
+         cerr << "Error reading input file " << argv[infile] << ": "
+              << strerror(errno) << endl;
+         exit(2);
+      }
+      else if(result != file_length)
+      {
+         cerr << "Short read count from input file " << argv[infile] << ": "
+              << strerror(errno) << endl;
+         exit(2);
+      }
+      close(in_fd);
+
+      //XOR with a constant to break Rockstar's advanced encryption.
+      uint32_t magic_number[] = {0xA1C43AEA,
+                                 0xF314A89A,
+                                 0x23D7B048,
+                                 0xF1FFE89D};
+
+      int decryption_state = 0;
+      for(uint32_t i = 0; i < (file_length / 4) + 1; ++i)
+      {
+         file_image[i] ^= magic_number[decryption_state];
+         decryption_state = (decryption_state + 1) % 4;
+      }
+
+      //Read and demux the Ogg stream.
+      int num_tracks = 1;
+      int num_bytes = 0;
+      uint8_t *current = (uint8_t *)file_image;
+      while(current < (uint8_t *)file_image + file_length)
+      {
+         cout << "   Track: " << num_tracks << endl;
+         ogg_header h;
+         read_header(current, &h);
+         current += sizeof(ogg_header);
+
+         ogg_entry e;
+         int i;
+
+         for(i = 0; i < 8; ++i)
+         {
+            //cout << "Entry: " << i << endl;
+            e = h.e[i];
+            if(e.size != 0xFFFFFFFF &&
+               e.size != 0xCDCDCDCD &&
+               e.foo != 0xCDCDCDCD)
+               break;
+         }
+
+         if(i >= 8)
+            continue;
+
+         //Sanity check
+         if(string((char *)current, 4) != "OggS")
+         {
+            cerr << "This doesn't look like an Ogg Vorbis stream to me.\n"
+                 << "Expected first 4 bytes to be \"0x"
+                 << hex
+                 << (unsigned int)'O' << (unsigned int)'g'
+                 << (unsigned int)'g' << (unsigned int)'S'
+                 << "\", found \"0x"
+                 << (unsigned int)current[0] << (unsigned int)current[1]
+                 << (unsigned int)current[2] << (unsigned int)current[3]
+                 << dec << "\"." << endl;
+            exit(8);
+         }
+
+         ostringstream outfilename;
+         outfilename << argv[argc - 1] << "/"
+                     << inbasename
+                     << "-"
+                     << setfill('0')
+                     << setw(3) << num_tracks
+                     << ".ogg";
+
+         int out_fd = creat(outfilename.str().c_str(), 0644);
+         if(out_fd == -1)
+         {
+            cerr << "Error opening output file for " << argv[infile] << " track "
+                 << num_tracks << ": " << strerror(errno) << endl;
+            exit(4);
+         }
+
+         int written_size = 0;
+         int result;
+         while(written_size < e.size)
+         {
+            result = write(out_fd, current, e.size - written_size);
+            if(result == -1)
+            {
+               cerr << "Error writing to output file for " << argv[infile] << " track "
+                    << num_tracks << ": " << strerror(errno) << endl;
+               exit(4);
+            }
+            written_size += result;
+            current += result;
+         }
+         close(out_fd);
+         ++num_tracks;
+      }
+   }
+}
