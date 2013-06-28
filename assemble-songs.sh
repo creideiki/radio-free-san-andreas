6ec2849ee8bd4241979ebf490af6d3a4175f8972
Karl-Johan Karlsson <creideiki@ferretporn.se>
2013-06-28 04:44:03 +0000

    Addd an option to remove song parts after assembly (from a concept by Jon Franklin).

--- assemble-songs.sh
+++ assemble-songs.sh
@@ -6,15 +6,17 @@ function usage
     echo "the Grand Theft Auto - San Andreas soundtrack into full"
     echo "songs, without any DJ chatter."
     echo ""
-    echo "Usage: $0 <dir>"
+    echo "Usage: $0 <dir> [--delete]"
     echo ""
     echo "Where dir is the top-level directory containing all radio"
     echo "stations extracted by Radio Free San Andreas (i.e. the same"
     echo "as the second to last argument to extract)."
+    echo "If --delete is specified, delete the partial songs and DJ"
+    echo "chatter after assembly, leaving only the full songs."
     exit 1
 }
 
-if [ $# != 1 ]
+if [ $# -eq 0 -o $# -gt 2 ]
 then
     usage
     exit 1
@@ -26,6 +28,18 @@ then
     echo ""
     usage
     exit 2
+fi
+
+DELETE=0
+if [ $# -eq 2 ]
+then
+    if [ "$2" = "--delete" ]
+    then
+        DELETE=1
+    else
+        usage
+        exit 1
+    fi
 fi
 
 cd "$1"
@@ -41,6 +55,11 @@ do
         then
             echo "Assembling song $base"
             cat "$base (Intro).ogg" "$base.ogg" "$base (Outro).ogg" > "$base (Complete).ogg"
+            if [ $DELETE -eq 1 ]
+            then
+                echo "Deleting leftover parts of song $base"
+                rm -f "$base"\ \(Intro*\).ogg "$base.ogg" "$base"\ \(Outro*\).ogg
+            fi
         fi
     done
     popd > /dev/null
